f195d19a9dafdb45a59d48e693529f90154d5716
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2025-11-03 22:07:20 -0300

    credit card interface

--- users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
+++ users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
@@ -1,4 +1,14 @@
 package br.edu.ufop.web.users.repository;
 
-public interface ICreditCardNetworkRepository {
+import br.edu.ufop.web.users.entity.CCNetworkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ICreditCardNetworkRepository extends JpaRepository<CCNetworkEntity, UUID> {
+
+    List<CCNetworkEntity> findByName(String name);
+    List<CCNetworkEntity> findByNameContainingIgnoreCase(String name);
+
 }
