02bd4748511a3d9e85b6b7b5b8977b9a5b24bbf5
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2025-11-17 22:06:28 -0300

    criando use case

--- users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
+++ users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
@@ -1,5 +1,24 @@
 package br.edu.ufop.web.users.domain.usecase;
 
-public class CreateUserUseCase {
+import br.edu.ufop.web.users.domain.UserDomain;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class CreateUserUseCase {
+    private UserDomain userDomain;
+
+    public void validate() {
+        // Validações conforme a regra de negócio para a criação do usuário
+        validateName();
+
+        // Demais validações
+    }
+
+    private void validateName() {
+        if (this.userDomain.getName() == null) {
+            throw new RuntimeException("Name is null");
+        }
+    }
 }
