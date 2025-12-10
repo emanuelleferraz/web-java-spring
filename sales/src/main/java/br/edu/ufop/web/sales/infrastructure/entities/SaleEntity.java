64a919e0bb0ff6326125a882f66c5fb675619810
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2025-12-10 20:12:51 -0300

    add infraestrutura e repository

---
+++ sales/src/main/java/br/edu/ufop/web/sales/infrastructure/entities/SaleEntity.java
@@ -0,0 +1,50 @@
+package br.edu.ufop.web.sales.infrastructure.entities;
+
+import br.edu.ufop.web.sales.enums.EnumSaleStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_sales")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private UUID userId;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private EventEntity event;
+
+    private LocalDateTime dateTime;
+
+    private EnumSaleStatus status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void beforeSave() {
+        this.status = EnumSaleStatus.EM_ABERTO;
+        this.dateTime = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
