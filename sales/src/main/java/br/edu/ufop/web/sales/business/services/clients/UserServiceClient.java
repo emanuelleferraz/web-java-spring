13ff878781167984106ed79068c76e1ab0160a49
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2025-12-10 23:31:22 -0300

    atualizando UserServiceClient

--- sales/src/main/java/br/edu/ufop/web/sales/business/services/clients/UserServiceClient.java
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/clients/UserServiceClient.java
@@ -3,11 +3,16 @@ package br.edu.ufop.web.sales.business.services.clients;
 import br.edu.ufop.web.sales.business.services.clients.dto.UserServiceDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.UUID;
 
 @FeignClient(name="users-service", url = "http://localhost:3000")
 public interface UserServiceClient {
     @GetMapping("/users")
     public List<UserServiceDTO> getAllUsers();
+
+    @GetMapping("/users/{id}")
+    public UserServiceDTO getById(@PathVariable(value = "id")UUID id);
 }
