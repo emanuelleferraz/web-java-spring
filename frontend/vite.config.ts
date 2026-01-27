d04911e5046a624802870c888fe6a3684fd78b32
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2026-01-26 22:08:44 -0300

    iniciando o frontend - add shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,8 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +10,10 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173
     },
-  plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
